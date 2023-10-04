862613383d44213ddc89e0a64744d8a7f23fd1ba
Thomas Kranitsas <thomas.kranitsas@topcoder.com>
2023-10-04 16:50:31 +0300

    use submittedDate if created is not available

--- src/services/SubmissionService.js
+++ src/services/SubmissionService.js
@@ -116,7 +116,7 @@ async function handle(event) {
       // Handle new submission
       logger.debug(`Started adding submission for ${event.payload.id}`);
       try {
-        const timestamp = Date.parse(event.payload.created);
+        const timestamp = Date.parse(event.payload.created || event.payload.submittedDate);
         const patchObject = await LegacySubmissionIdService.addSubmission(
           event.payload.id,
           event.payload.challengeId,
@@ -177,7 +177,7 @@ async function handle(event) {
     // we will try to create it here
     logger.debug(`Started adding submission for ${event.payload.id}`);
     try {
-      const timestamp = Date.parse(event.payload.created);
+      const timestamp = Date.parse(event.payload.created || event.payload.submittedDate);
       const patchObject = await LegacySubmissionIdService.addSubmission(
         event.payload.id,
         event.payload.challengeId,
